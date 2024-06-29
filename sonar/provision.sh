cb38b02a3df214ab9cb6de32f46155b1a0398f56
Rafael Garcia SantAnna <rgarciasantanna@gmail.com>
2024-06-29 12:15:45 -0300

    Finalizando Parte do sonar

---
+++ sonar/provision.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# useradd sonar
+# yum install wget java-11-openjdk-devel unzip -y
+
+# wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.1.0.47736.zip
+
+# unzip sonarqube-9.1.0.47736.zip -d /opt/
+
+# mv /opt/sonarqube-9.1.0.47736/ /opt/sonarqube
+
+# chown -R sonar:sonar /opt/sonarqube
+
+# touch /etc/systemd/system/sonar.service
+# echo > /etc/systemd/system/sonar.service
+# cat <<EOT >> /etc/systemd/system/sonar.service
+# [Unit]
+# Description=Sonarqube service
+# After=syslog.target network.target
+# [Service]
+# Type=forking
+# ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+# ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+# User=sonar
+# Group=sonar
+# Restart=always
+# [Install]
+# WantedBy=multi-user.targvet
+# EOT
+
+# sudo systemctl daemon-reload
+
+# service sonar start
+
+
+#Install sonar scanner
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
+unzip sonar-scanner-cli-4.6.2.2472-linux.zip -d /opt/
+mv /opt/sonar-scanner-4.6.2.2472-linux /opt/sonar-scanner
+chown -R sonar:sonar /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+
+
+#install nodejs
+curl –sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+
+sudo yum install –y nodejs
