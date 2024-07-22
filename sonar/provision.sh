6d5de9b5fbab1e2d42678c554af3865ab1055844
Rafael Garcia SantAnna <rgarciasantanna@gmail.com>
2024-07-22 20:22:30 -0300

    Atualizando a maquina do sonar

--- sonar/provision.sh
+++ sonar/provision.sh
@@ -1,35 +1,65 @@
 #!/bin/bash
-# useradd sonar
-# yum install wget java-11-openjdk-devel unzip -y
 
-# wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.1.0.47736.zip
+##########################################################################
 
-# unzip sonarqube-9.1.0.47736.zip -d /opt/
+# Set DNS manually
+echo "Setting DNS..."
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-# mv /opt/sonarqube-9.1.0.47736/ /opt/sonarqube
+# Update system and install necessary packages
+echo "Updating system and installing necessary packages..."
+dnf clean all
+dnf makecache
+dnf -y update
 
-# chown -R sonar:sonar /opt/sonarqube
+# Fix repository issues by updating the base URL
+echo "Fixing repository issues..."
+sed -i 's|^mirrorlist=|#mirrorlist=|g' /etc/yum.repos.d/CentOS-*
+sed -i 's|^#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
-# touch /etc/systemd/system/sonar.service
-# echo > /etc/systemd/system/sonar.service
-# cat <<EOT >> /etc/systemd/system/sonar.service
-# [Unit]
-# Description=Sonarqube service
-# After=syslog.target network.target
-# [Service]
-# Type=forking
-# ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
-# ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
-# User=sonar
-# Group=sonar
-# Restart=always
-# [Install]
-# WantedBy=multi-user.targvet
-# EOT
+# Update repositories and install httpd
+echo "Installing Apache HTTP server..."
+dnf -y update
+dnf -y install httpd
 
-# sudo systemctl daemon-reload
+# Enable and start httpd service
+echo "Enabling and starting httpd service..."
+systemctl enable httpd
+systemctl start httpd
+##########################################################################
 
-# service sonar start
+useradd sonar
+yum update -y
+yum install wget java-11-openjdk-devel unzip -y
+
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.1.0.47736.zip
+
+unzip sonarqube-9.1.0.47736.zip -d /opt/
+
+mv /opt/sonarqube-9.1.0.47736/ /opt/sonarqube
+
+chown -R sonar:sonar /opt/sonarqube
+
+touch /etc/systemd/system/sonar.service
+echo > /etc/systemd/system/sonar.service
+cat <<EOT >> /etc/systemd/system/sonar.service
+[Unit]
+Description=Sonarqube service
+After=syslog.target network.target
+[Service]
+Type=forking
+ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+User=sonar
+Group=sonar
+Restart=always
+[Install]
+WantedBy=multi-user.targvet
+EOT
+
+sudo systemctl daemon-reload
+
+service sonar start
 
 
 #Install sonar scanner
